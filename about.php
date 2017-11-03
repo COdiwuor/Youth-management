a8e6ff2edf7b38cf1373b110b8a972f7b90e592b
Oppal <32040208+Oppal@users.noreply.github.com>
2017-11-03 15:56:22 +0300

    Add files via upload

--- about.php
+++ about.php
@@ -6,38 +6,29 @@
 rel="stylesheet"
 type="text/css"
 href="ycsms.css"/>
-<div>
-    <nav >
-<a ><img class="img" src="contact.PNG" style="width:2%;height:2%;">07112228222</a>
-<a style="float:right" href="www.facebook.com"><img class="img" src="facebook.ico" style="width:10%;height:10%;"></a>
-<a style="float:right"href="www.twitter.com"><img src="twitter.PNG" style="width:10%;height:10%;"></a>
-<a style="float:right"href="www.youtube.com" ><img src="youtube.JPG" style="width:10%;height:10%;"></a>
-</nav>
 
 
-<hr>
-</div>
 <div class="container-full joinbar">
 <div class="main"style="background-color:white; font-size:20px"> 
-	<header><nav><div class style="background-image:url(wbb.PNG); color:white"><img src="logo.png"style="width:100px; height:100px;"><a href="index.php"style="padding:50px;color:black;hover:red;text-decoration: none;">Home</a><a href="about.php"style="color:black; padding:50px;text-decoration: none;">About</a><a href="join.php"style="padding:25px; color:black;text-decoration: none;">How to Join</a><a href="contact.html"style="padding:25px;color:black;text-decoration: none;">Contact Us</a><a href="login.php"style="padding:25px;color:black;text-decoration: none;">Login</a></nav></header>
+	<header><nav><div class style="background-image:url(wbb.PNG); color:white"><img src="logo.png"style="width:100px; height:100px;"><a href="index.php"style="padding:50px;color:black;hover:red;text-decoration: none;">Home</a><a href="about.php"style="color:black; padding:50px;text-decoration: none;">About</a><a href="join.php"style="padding:25px; color:black;text-decoration: none;">How to Join</a><a href="contact.html"style="padding:25px;color:black;text-decoration: none;">Contact Us</a><a href="login.html"style="padding:25px;color:black;text-decoration: none;">Login</a></nav></header>
 
 
 </div>
 </div>
     </head>
-    <body>
+    <body style="background-image:url(background6jpg.JPG)">
         <br>
-        <br>
+
+        
 <fieldset style="background-color:grey">
-    <div  style="text-align:center; font-size: 200%"><strong>Jitolee Youth Services</strong></div><br>
+    <div  style="text-align:center; font-size: 200%; color:red-black"><strong>Jitolee Youth Services</strong></div><br>
     <div style="text-align:center; font-size: 150%"> Jitolee Youth Service is a programme by Nairobi county government Ministry of Environment and Natural Resources that aims to create a conducive environment for every person in Nairobi by providing community services to voluntary youth.<br><br>
     Jitolee Youth Service programme is the stepping stone towards providing a source of income to the idle youth and as well ensuring a maintained clean environment.<br><br>
     Youths will be abe to join and book a work slot from anywhere they are without any literal qualifications needed to join.
 
         </p>
     </fieldset>
-
-
+</body>
 
 
 
